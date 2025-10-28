2ce769a55ff08cf0d7e4e84087a02575963abb6b
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-10-28 10:28:49 -0500

    chore: initialize blank Java project with 'Maven'.

---
+++ challenge_conversor_monedas/src/main/java/a1t0ghb/courses_oracle_one/challenge_conversor_monedas/App.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+//  Package location definition.
+package a1t0ghb.courses_oracle_one.challenge_conversor_monedas;
+
+//  IMPORTS: external classes and interfaces.
+
+//  IMPORTS - UTILITIES.
+//  Shortcut for importing ALL Java Utils: 'import java.util.*;'.
+
+//  IMPORTS - CUSTOM CLASSES AND CUSTOM INTERFACES.
+
+/**
+ *
+ * @author a1t0ghb
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //  INITIALIZATION OF INSTANCES.
+
+        
+
+        //  ATTRIBUTES DECLARATION.
+
+        //  CONSTRUCTOR.
+        //  - Same name as Class, and custom definition.
+        //  - If 'empty' (i.e. 'predetermined'), it's usually useful to at least initialize attributes with default values.
+        //  - If we define a constructor for the superclass, now ALL OF ITS SUBCLASSES must have a MATCHING constructor.
+
+        //  ATTRIBUTE'S 'GETTERS'.
+
+        //  ATTRIBUTE'S 'SETTERS': usually, public.
+        //  - Protected keyword 'this' is used to refer to CURRENT object, to avoid confusions with parameters (in case those have the same name).
+
+        //  INHERITED METHODS TO OVERRIDE: custom definitions for CURRENT class.
+
+        //  METHODS FROM 'IMPLEMENTS'.
+        
+        //  REGULAR METHODS.
+
+    }
+}
+//  EOF.
