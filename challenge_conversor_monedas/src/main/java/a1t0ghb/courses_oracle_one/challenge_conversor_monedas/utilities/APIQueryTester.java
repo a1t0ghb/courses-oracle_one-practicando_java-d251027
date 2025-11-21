a7fdabdbadd0f35419f79eabe9bef1da6bc579c4
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-11-21 23:25:43 +0000

    feat: add Java class 'APIQueryTester.java' for quick testing of 'APIQuery' instances.

---
+++ challenge_conversor_monedas/src/main/java/a1t0ghb/courses_oracle_one/challenge_conversor_monedas/utilities/APIQueryTester.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package a1t0ghb.courses_oracle_one.challenge_conversor_monedas.utilities;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author a1t0ghb
+ */
+public class APIQueryTester {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        System.out.println();
+
+        var my_api_query = new APIQuery();
+        System.out.println("-");
+        // my_api_query.GetCurrencyData("my_api_token", "my_currency_from");       //  HTTP response: 404 (Page not found). Server didn't find a page, a returns a page for 'broken / dead links'.
+        my_api_query.GetCurrencyData("anytoken", "USD");                        //  HTTP response: 403 (Forbidden access). Server refused to process request, sending a valid JSON response, but referring to forbidden access.
+        System.out.println();
+
+        System.out.println("-");
+        System.out.println();
+
+    }
+
+}
