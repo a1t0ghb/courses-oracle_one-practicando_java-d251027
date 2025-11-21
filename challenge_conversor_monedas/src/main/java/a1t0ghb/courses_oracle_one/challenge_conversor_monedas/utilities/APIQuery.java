2c2436acbba46a1e423d4c6974fb5f5f078794c1
a1t0git <github_ctc101@a1t0.aleeas.com>
2025-11-21 20:16:15 +0000

    feat: create plain Java class 'APIQuery.java' for managing API queries with HTTP requests..

---
+++ challenge_conversor_monedas/src/main/java/a1t0ghb/courses_oracle_one/challenge_conversor_monedas/utilities/APIQuery.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+//  Package location definition.
+package a1t0ghb.courses_oracle_one.challenge_conversor_monedas.utilities;
+
+//  IMPORTS: external classes and interfaces.
+
+//  IMPORTS - UTILITIES.
+
+//  Java utilities:
+//  - Shortcut for importing ALL Java Utils: 'import java.util.*;'.
+
+//  IMPORTS - CUSTOM CLASSES AND CUSTOM INTERFACES.
+
+/**
+ * APIQuery class documentation.
+ * @author a1t0ghb
+ */
+public class APIQuery {
+
+    //  ATTRIBUTES DECLARATION.
+    //  - IMPORTANT: regarding primitive types (e.g. int, boolean, etc.), try ALWAYS instead to use their corresponding wrapper classes (e.g. Integer instead of int), to use proper methods, such as comparing and avoiding un-expected behaviours.
+    //  - Example of un-intended behaviours: comparing integers. Ref.: 'https://medium.com/@wanisha2013/always-use-equals-to-compare-integer-objects-in-java-e35e469332aa'.
+    //  - Wrappers for primitives, for proper use of methods, such as '.equals()', or 'compare()': 'https://www.w3schools.com/java/java_wrapper_classes.asp', 'https://www.geeksforgeeks.org/java/wrapper-class-vs-primitive-data-types-in-java-with-io/'.
+
+    //  CONSTRUCTOR.
+    //  - Same name as Class, and custom definition.
+    //  - If 'empty' (i.e. 'predetermined'), it's usually useful to at least initialize attributes with default values.
+    //  - If we define a constructor for the superclass, now ALL OF ITS SUBCLASSES must have a MATCHING constructor.
+    //  - [RECORDS] Custom constructor (e.g. canonical, compact, etc.): 'https://www.baeldung.com/java-record-keyword#constructors'.
+
+    //  ATTRIBUTE'S 'GETTERS'.
+
+    //  ATTRIBUTE'S 'SETTERS': usually, public.
+    //  - Protected keyword 'this' is used to refer to CURRENT object, to avoid confusions with parameters (in case those have the same name).
+
+    //  INHERITED METHODS TO OVERRIDE: custom definitions for CURRENT class.
+
+    //  METHODS FROM 'IMPLEMENTS'.
+    
+    //  REGULAR METHODS.
+
+    // public static void main(String[] args) {
+    //     // System.out.println("Hello World!");
+
+    //     //  INITIALIZATION OF INSTANCES.
+
+    // }
+
+}
